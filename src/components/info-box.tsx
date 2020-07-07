07e39abf957e1b078df0027cb233144bd8d17bdd
Simon <simonjturk@gmail.com>
2020-07-08 08:03:09 +1000

    fixed info box

--- src/components/info-box.tsx
+++ src/components/info-box.tsx
@@ -2,10 +2,12 @@
 import { jsx } from "theme-ui";
 
 import React from "react";
+import MarkDown from "./utilities/markdown";
+import { Markdown } from "./interfaces/markdown";
 
 export interface InfoBoxProps {
   heading: string;
-  message: string;
+  message: string | Markdown;
 }
 
 // Render inline SVG with fallback non-svg images
@@ -13,13 +15,7 @@ const InfoBox: React.FC<InfoBoxProps> = ({ heading, message }) => {
   return (
     <div className="w-1/4 flex flex-col items-center text-center">
       <div sx={{ color: "textLight", fontSize: [4, 4, 6] }}> {heading} </div>
-      <div
-        sx={{ color: "textLight", fontSize: [1, 1, 4] }}
-        className="whitespace-pre-wrap"
-        dangerouslySetInnerHTML={{
-          __html: message,
-        }}
-      />
+      <MarkDown sx={{ color: "textLight", fontSize: [1, 1, 4]}} data={message}></MarkDown>
     </div>
   );
 };
