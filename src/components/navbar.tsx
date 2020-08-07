23767cfe1823ae1e883e969f1b7a27542c4180d5
Simon <simonjturk@gmail.com>
2020-08-07 11:40:31 +1000

    farting about with the navbar image

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -34,7 +34,10 @@ const NavBar: React.FC<Props> = (props) => {
 
   return (
     <Fragment>
-      <div sx={{variant: scrolled ? 'menu.scrolled' : 'menu' }} className="fixed z-40 w-full">
+      <div
+        sx={{ variant: scrolled ? "menu.scrolled" : "menu" }}
+        className="fixed z-40 w-full"
+      >
         <div
           sx={{ height: ["22px", "40px", "60px"] }}
           className="pt-1 lg:pt-4 flex flex-wrap justify-end items-center"
@@ -43,8 +46,9 @@ const NavBar: React.FC<Props> = (props) => {
         </div>
         <svg
           className="block fixed "
+          version="1.1"
+          viewBox="0 0 442.38 29.115"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1690 118.67"
         >
           <defs>
             <linearGradient id="g1">
@@ -53,16 +57,23 @@ const NavBar: React.FC<Props> = (props) => {
               <stop offset="100%" stopColor="#ffffff" stopOpacity=".3" />
             </linearGradient>
           </defs>
-          <path
-            d="M0 63.855V9.044l3.667.89c6.154 1.492 32.2 14.443 47 23.38 7.7 4.646 25.4 16.103 39.333 25.46 58.697 39.42 101.4 54.634 154 54.86 58.438.25 101.6-16.84 168.67-66.77 21.03-15.658 40.262-26.223 54.672-30.032 4.403-1.164 13.703-2.716 20.667-3.45 15.12-1.592 261.17-1.74 811.66-.49l391 .888v104.9H-.03z"
-            fill="#ffffff"
-            fillOpacity="0"
-          />
-          <path
-            d="M227.33 113.35c-46.254-4.063-85.1-19.5-137.33-54.58C50.2 32.043 20.178 14.955 5.06 10.426.514 9.064 0 8.457 0 4.454V-.001h1690.7v13.57l-593-.95c-436.7-.7-596.87-.553-607.7.555-28.857 2.954-45.998 10.427-77.306 33.706-46.95 34.9-77.93 51.376-113.35 60.244-23.686 5.93-49.713 8.182-71.988 6.225z"
-            fill={scrolled ? "#ffffff" : "url(#g1)"}
-          />
+
+         
+          <g>
+            <title>Layer 1</title>
+            <g stroke="null" id="svg_1">
+              <g stroke="null" stroke-width="0.35278" id="svg_2">
+                <path
+                  stroke="null"
+                  d="m58.64595,28.68032c-12.28411,-1.09028 -22.60083,-5.23344 -36.47289,-14.64766c-3.70039,-2.51113 -8.40123,-5.58551 -10.44621,-6.83179c-3.84794,-2.34519 -11.17394,-6.06933 -11.95086,-6.07511c-0.24344,-0.00203 -0.44263,-0.40584 -0.44263,-0.89786l0,-0.89456l444.04664,0l0,2.56479l-155.5438,-0.25335c-114.61017,-0.18667 -156.56764,-0.14704 -159.44846,0.15063c-7.66487,0.79348 -12.21685,2.79899 -20.53106,9.04613c-12.4688,9.36878 -20.69668,13.78752 -30.10198,16.16811c-6.29051,1.59166 -13.20255,2.19578 -19.11876,1.67067l0.01001,-0.00001l0,0.00001z"
+                  fill={scrolled ? "#ffffff" : "url(#g1)"}
+                  id="svg_3"
+                />
+              </g>
+            </g>
+          </g>
         </svg>
+
         <img
           sx={{
             position: "absolute",
@@ -85,6 +96,31 @@ const NavBar: React.FC<Props> = (props) => {
 export default NavBar;
 
 /*
+
+    <svg
+          className="block fixed "
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 445.56 30.339"
+        >
+          <defs>
+            <linearGradient id="g1">
+              <stop stopColor="#ffffff" />
+              <stop offset="20%" stopColor="#ffffff" stopOpacity=".4" />
+              <stop offset="100%" stopColor="#ffffff" stopOpacity=".3" />
+            </linearGradient>
+          </defs>
+          <g transform="translate(145.58 -154.5)">
+            <g transform="translate(0 41.536)" stroke-width=".35278">
+              <path
+                d="m-86.142 142.25c-12.238-1.0749-22.516-5.1596-36.336-14.441-10.107-6.7878-17.713-11.164-22.001-12.659-0.84971-0.29621-1.1059-0.59405-1.1059-1.2858v-0.9003h445.56v2.8839l-156.37-0.25075c-115.17-0.18468-157.39-0.14588-160.26 0.14726-7.6353 0.78168-12.17 2.759-20.454 8.9183-12.422 9.2366-20.619 13.593-29.989 15.94-6.2669 1.5692-13.153 2.1648-19.047 1.6471z"
+                fill={scrolled ? "#ffffff" : "url(#g1)"}
+              />
+            </g>
+          </g>
+        </svg>
+
+
+
 sx={{ height: ["40px", "40px", "70px"] }}
  sx={{
               width: ["20%", "20vw", "20vw"],
