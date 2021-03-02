83494426a6fd124ae50932b3dd252e8000edd75f
Simon <simonjturk@gmail.com>
2021-03-02 12:08:42 +1100

    as per client request

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -7,6 +7,8 @@ export interface CtaProps {
   link: string;
   ctaMessage: any;
 }
+const html = ""; //ctaMessage.childMarkdownRemark.html,
+
 const CTA: React.FC<CtaProps> = ({ buttonLabel, link, ctaMessage }) => (
   <div sx={{ variant: "gradients.gray" }}>
     <div
@@ -18,18 +20,19 @@ const CTA: React.FC<CtaProps> = ({ buttonLabel, link, ctaMessage }) => (
       }}
       className="flex flex-wrap flex-col text-center lg:text-left lg:flex-row lg:justify-between  items-center"
     >
-      <div
-        sx={{ variant: "cta" }}
-        className="whitespace-pre-wrap"
-        contentEditable="true"
-        dangerouslySetInnerHTML={{
-          __html: ctaMessage.childMarkdownRemark.html,
-        }}
-      />
+      {html === "" && (
+        <div
+          sx={{ variant: "cta" }}
+          className="whitespace-pre-wrap"
+          contentEditable="true"
+          dangerouslySetInnerHTML={{
+            __html: html,
+          }}
+        />
+      )}
       <a sx={{ variant: "buttons.cta" }} href={link} target="_blank">
         {buttonLabel}
       </a>
-      
     </div>
   </div>
 );
